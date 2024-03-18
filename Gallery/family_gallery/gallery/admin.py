0b3f07251fcf0d55c38b0eb6c9de912b9c25e73b
ashaikeus <ashaikeus@gmail.com>
2024-03-18 21:32:45 +0300

    update models

--- Gallery/family_gallery/gallery/admin.py
+++ Gallery/family_gallery/gallery/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+admin.site.register(Photo)
+# admin.site.register(Person)
+# admin.site.register(Tag)
+# admin.site.register(Comment)
+# admin.site.register(Album)
+#
+# admin.site.register(PhotoTags)
+# admin.site.register(PhotoPerson)
+# admin.site.register(PhotoLikes)
+#
+# admin.site.register(ParentsChildren)
+#
+# admin.site.register(AlbumPhotos)
+# admin.site.register(AlbumCircles)
--- Gallery/family_gallery/gallery/models.py
+++ Gallery/family_gallery/gallery/models.py
@@ -1,69 +1,125 @@
 from django.db import models
 from django.utils import timezone
+from datetime import datetime
+from django.db.models import Q
 
 
-class Photo(models.model):
-    link_to_image = models.TextField(null=False)
-    people = models.ManyToManyField('Person', through='PhotoPerson', null=False)
+class Photo(models.Model):
+    link_to_image = models.URLField(blank=False)
+    # people = models.ManyToManyField('Person', through='PhotoPerson', related_name='photos_with')
+    date = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=75)
+    description = models.TextField()
+    place = models.CharField(max_length=75)
+    # tags = models.ManyToManyField('Tag', through='PhotoTags')
+
+    # likes = models.ManyToManyField('Person', through='PhotoLikes', related_name='photos_liked_by')
+    # owner = models.ForeignKey('Person', on_delete=models.SET_NULL, blank=True, null=True)
+    upload_date = models.DateTimeField(default=timezone.now)
+    # trusted_users = models.ManyToManyField('Person', through='PhotoTrust')
+    album_id = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
+
+
+# complex stuff, I'll return to it later
+'''
+class Photo(models.Model):
+    link_to_image = models.TextField(blank=False)
+    people = models.ManyToManyField('Person', through='PhotoPerson',
+                                    related_name='photos_with')
     date = models.DateTimeField(default=timezone.now)
     name = models.CharField(max_length=75)
     description = models.TextField()
     place = models.CharField(max_length=75)
     tags = models.ManyToManyField('Tag', through='PhotoTags')
 
-    likes = models.ManyToManyField('Person', through='PhotoLikes')
-    comments = models.ManyToManyField('Person', through='PhotoComments')
-    owner = models.ForeignKey('Person', on_delete=models.SET_NULL)
+    likes = models.ManyToManyField('Person', through='PhotoLikes', related_name='photos_liked_by')
+    owner = models.ForeignKey('Person', on_delete=models.SET_NULL, blank=True, null=True)
     upload_date = models.DateTimeField(default=timezone.now)
     # trusted_users = models.ManyToManyField('Person', through='PhotoTrust')
     album_id = models.IntegerField(default=0)
 
+    def __str__(self):
+        return self.name
 
-class Person(models.model):
+
+class Person(models.Model):
     name = models.CharField(max_length=30)
     surname = models.CharField(max_length=50)
-    link_to_avatar = models.TextField()
-    gender = models.CharField(max_length=1, choices=(("m", "Мужчина"), ("f", "Женщина")))
-    birthdate = models.DateTimeField()
-    city = models.CharField(max_length=50)
-    mother = models.ForeignKey('Person', on_delete=models.SET_NULL, limit_choices_to={"gender": "f"})
-    father = models.ForeignKey('Person', on_delete=models.SET_NULL, limit_choices_to={"gender": "m"})
+    link_to_avatar = models.TextField(blank=True)
+    gender = models.CharField(max_length=1, blank=True, choices=(("m", "Мужчина"), ("f", "Женщина")))
+    birthdate = models.DateTimeField(default=datetime(1000, 1, 1))
+    city = models.CharField(max_length=50, blank=True)
+    mother = models.ForeignKey('Person', on_delete=models.SET_NULL, null=True,
+                               related_name='mother_to', limit_choices_to={"gender": "f"}, blank=True)
+    father = models.ForeignKey('Person', on_delete=models.SET_NULL, null=True,
+                               related_name='father_to', limit_choices_to={"gender": "m"}, blank=True)
     children = models.ManyToManyField('Person', through='ParentsChildren')
-    spouse = models.ForeignKey('Person', on_delete=models.SET_NULL, limit_choices_to={"gender__ne": gender})
+    spouse = models.ForeignKey('Person', on_delete=models.SET_NULL, null=True, blank=True,
+                               related_name='current_spouse',
+                               limit_choices_to=Q(gender='m') if gender == 'f' else Q(gender='f'))
     # exes = models.ManyToManyField
 
+    def __str__(self):
+        return self.name + ' ' + self.surname
 
-class Tag(models.model):
+
+class Tag(models.Model):
     name = models.CharField(max_length=70)
 
+    def __str__(self):
+        return self.name
 
-class Comment(models.model):
-    author = models.ForeignKey('Person', on_delete=models.SET_NULL)
+
+class Comment(models.Model):
+    photo = models.ForeignKey('Photo', on_delete=models.CASCADE, blank=False)
+    author = models.ForeignKey('Person', on_delete=models.SET_NULL, blank=True, null=True)
     text = models.TextField()
+
+    def __str__(self):
+        return self.author.name + ' - ' + self.text
+
+
+class Album(models.Model):
+    name = models.CharField(max_length=75)
+    description = models.TextField()
+    photos = models.ManyToManyField('Photo', through='AlbumPhotos')
+    circle = models.ManyToManyField('Person', through='AlbumCircles')
+
+    def __str__(self):
+        return self.name
 
 
 # Many-to-many relationship tables
 
-class PhotoPerson(models.model):
-    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
-    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+class PhotoPerson(models.Model):
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name='people_in_photo')
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='photos_with_person')
 
 
-class PhotoTags(models.model):
-    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
-    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+class PhotoTags(models.Model):
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name='tags_for_photo')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE, related_name='photos_with_tag')
 
 
-class PhotoLikes(models.model):
-    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
-    like = models.ForeignKey(Person, on_delete=models.CASCADE)
+class PhotoLikes(models.Model):
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name='likes_for_photo')
+    like = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='liked_by_person')
 
 
-class PhotoComments(models.model):
-    photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
-    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+class ParentsChildren(models.Model):
+    parent = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='children_to')
+    child = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='parent_to')
 
 
-class ParentsChildren(models.model):
-    parent = models.ForeignKey(Person, on_delete=models.CASCADE)
-    child = models.ForeignKey(Person, on_delete=models.CASCADE)
+class AlbumPhotos(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='photos_in_album')
+    photo = models.ForeignKey(Photo, on_delete=models.CASCADE, related_name='album_for_photo')
+
+
+class AlbumCircles(models.Model):
+    album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='circle_for_album')
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='albums_for_circle')
+'''
